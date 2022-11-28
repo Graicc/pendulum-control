be0c14bef20b1ae97d37b9aec09a4c00a19ada89
Graicc <33105645+Graicc@users.noreply.github.com>
2022-11-28 17:04:34 -0500

    Add graphs, integral clamping

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use bevy::{input::mouse::MouseMotion, prelude::*, sprite::MaterialMesh2dBundle};
-use bevy_egui::{egui, EguiContext, EguiPlugin};
+use bevy_egui::{
+    egui::{
+        self,
+        plot::{Legend, Line, Plot, PlotPoints},
+    },
+    EguiContext, EguiPlugin,
+};
 use bevy_prototype_debug_lines::*;
 use std::f32::consts::PI;
 
@@ -17,6 +23,7 @@ fn main() {
         .add_system(move_pendulum)
         .add_system(draw_pendulum)
         .add_system(debug_draw)
+        .add_system(history)
         .run();
 }
 
@@ -27,6 +34,7 @@ struct Pendulum {
     length: f32,
     control: f32,
     control_power: f32,
+    control_history: Vec<f32>,
 }
 
 impl Pendulum {
@@ -41,13 +49,16 @@ impl Pendulum {
     }
 }
 
-#[derive(Component)]
+#[derive(Component, Default)]
 struct PID {
     set_point: f32,
     proportional_gain: f32,
     integral_gain: f32,
     derivative_gain: f32,
     accumulator: f32,
+    accumulator_enabled: bool,
+    error_history: Vec<f32>,
+    accumulator_history: Vec<f32>,
 }
 
 const DT: f32 = 0.05;
@@ -71,11 +82,12 @@ fn add_pendulum(
             ..default()
         },
         PID {
-            accumulator: 0.0,
-            set_point: PI,
+            // set_point: PI,
+            set_point: 4.3,
             proportional_gain: -8.0,
-            integral_gain: 0.0,
+            integral_gain: -5.5,
             derivative_gain: -4.0,
+            ..default()
         },
         MaterialMesh2dBundle {
             mesh: meshes.add(shape::Circle::new(1.).into()).into(),
@@ -102,6 +114,19 @@ fn draw_pendulum(mut query: Query<(&mut Transform, &Pendulum)>) {
     for (mut transform, pendulum) in query.iter_mut() {
         let (x, y) = pendulum.to_rectangular();
         transform.translation = Vec3::new(x, y, 0.0);
+    }
+}
+
+fn history(mut query: Query<(&mut Pendulum, &mut PID)>) {
+    for (mut pendulum, mut pid) in query.iter_mut() {
+        let error = pid.set_point - pendulum.a;
+
+        pid.error_history.push(error);
+        let acc = pid.accumulator;
+        pid.accumulator_history.push(acc);
+
+        let control = pendulum.control;
+        pendulum.control_history.push(control);
     }
 }
 
@@ -149,13 +174,26 @@ fn control_pendulum_mouse(
 fn control_pendulum_pid(mut query: Query<(&mut Pendulum, &mut PID)>) {
     for (mut pendulum, mut pid) in query.iter_mut() {
         // proportional
-        let prop = (pendulum.a - pid.set_point) * pid.proportional_gain;
-
-        // integral
-        pid.accumulator += (pendulum.a - pid.set_point) * pid.integral_gain * DT;
+        let error = pendulum.a - pid.set_point;
+        let prop = error * pid.proportional_gain;
 
         // derivative
         let der = pendulum.da * pid.derivative_gain;
+
+        let control = prop + der;
+
+        // integral
+        if error.abs() < 0.1 {
+            pid.accumulator_enabled = true;
+        }
+
+        if pid.accumulator_enabled {
+            pid.accumulator += error * pid.integral_gain * DT;
+            // pid.accumulator = pid.accumulator.clamp(-1.0, 1.0);
+            pid.accumulator = pid
+                .accumulator
+                .clamp((-1.0 - control).min(0.0), (1.0 - control).max(0.0));
+        }
 
         let control = prop + pid.accumulator + der;
 
@@ -179,44 +217,87 @@ fn ui_example(
     mut egui_context: ResMut<EguiContext>,
     mut query: Query<(&mut Pendulum, Option<&mut PID>)>,
 ) {
-    egui::Window::new("Pendulum settings").show(egui_context.ctx_mut(), |ui| {
-        for (mut pendulum, mut pid) in query.iter_mut() {
-            ui.label("Pendulum");
-            ui.add(egui::Slider::new(&mut pendulum.length, 0.0..=20.0).text("length"));
-            ui.add(
-                egui::Slider::new(&mut pendulum.control_power, 0.0..=20.0).text("control_power"),
-            );
+    egui::Window::new("Pendulum settings")
+        .resizable(true)
+        .show(egui_context.ctx_mut(), |ui| {
+            for (mut pendulum, mut pid) in query.iter_mut() {
+                ui.label("Pendulum");
+                ui.add(egui::Slider::new(&mut pendulum.length, 0.0..=20.0).text("length"));
+                ui.add(
+                    egui::Slider::new(&mut pendulum.control_power, 0.0..=20.0)
+                        .text("Control power"),
+                );
 
-            ui.label(format!("{}", pendulum.control));
+                ui.label(format!("{}", pendulum.control));
 
-            if ui.button("Reset").clicked() {
-                pendulum.a = PI;
-                pendulum.da = 0.05;
-                if let Some(pid) = &mut pid {
-                    pid.accumulator = 0.0;
+                if ui.button("Reset").clicked() {
+                    pendulum.a = PI;
+                    pendulum.da = 0.05;
+                    pendulum.control_history = Vec::new();
+                    if let Some(pid) = &mut pid {
+                        pid.accumulator = 0.0;
+                        pid.accumulator_enabled = false;
+                        pid.error_history = Vec::new();
+                        pid.accumulator_history = Vec::new();
+                    }
+                }
+
+                let slider_range = 10.0;
+
+                if let Some(mut pid) = pid {
+                    ui.separator();
+                    ui.label("PID");
+                    ui.label(format!("Error: {}", pendulum.a - pid.set_point));
+                    // TODO: There's probably a better way to do this
+                    let old_set_point = pid.set_point;
+                    ui.add(egui::Slider::new(&mut pid.set_point, 0.0..=2.0 * PI).text("Set point"));
+                    if pid.set_point != old_set_point {
+                        pid.accumulator = 0.0;
+                        pid.accumulator_enabled = false;
+                    }
+                    ui.add(
+                        egui::Slider::new(&mut pid.proportional_gain, -slider_range..=slider_range)
+                            .text("Proportional gain"),
+                    );
+                    ui.add(
+                        egui::Slider::new(&mut pid.integral_gain, -slider_range..=slider_range)
+                            .text("Integral gain"),
+                    );
+                    ui.add(
+                        egui::Slider::new(&mut pid.derivative_gain, -slider_range..=slider_range)
+                            .text("Derivative gain"),
+                    );
+
+                    let error_points: PlotPoints = pid
+                        .error_history
+                        .iter()
+                        .enumerate()
+                        .map(|(i, v)| [(i as f64) * (DT as f64), *v as f64])
+                        .collect();
+
+                    let accumulator_points: PlotPoints = pid
+                        .accumulator_history
+                        .iter()
+                        .enumerate()
+                        .map(|(i, v)| [(i as f64) * (DT as f64), *v as f64])
+                        .collect();
+
+                    let control_points: PlotPoints = pendulum
+                        .control_history
+                        .iter()
+                        .enumerate()
+                        .map(|(i, v)| [(i as f64) * (DT as f64), *v as f64])
+                        .collect();
+
+                    Plot::new("My Plot")
+                        .legend(Legend::default())
+                        .view_aspect(2.0)
+                        .show(ui, |plot_ui| {
+                            plot_ui.line(Line::new(error_points).name("Error"));
+                            plot_ui.line(Line::new(accumulator_points).name("Accumulator"));
+                            plot_ui.line(Line::new(control_points).name("Control"));
+                        });
                 }
             }
-
-            let slider_range = 10.0;
-
-            if let Some(mut pid) = pid {
-                ui.add_space(10.0);
-                ui.label("PID");
-                ui.label(format!("Error: {}", pendulum.a - pid.set_point));
-                ui.add(egui::Slider::new(&mut pid.set_point, 0.0..=2.0 * PI).text("Set point"));
-                ui.add(
-                    egui::Slider::new(&mut pid.proportional_gain, -slider_range..=slider_range)
-                        .text("Proportional gain"),
-                );
-                ui.add(
-                    egui::Slider::new(&mut pid.integral_gain, -slider_range..=slider_range)
-                        .text("Integral gain"),
-                );
-                ui.add(
-                    egui::Slider::new(&mut pid.derivative_gain, -slider_range..=slider_range)
-                        .text("Derivative gain"),
-                );
-            }
-        }
-    });
+        });
 }
